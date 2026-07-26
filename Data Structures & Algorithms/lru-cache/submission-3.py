5f2fae78d1bc3e4ddff39535915cc75d981256f8
akash <58539815+AMOORCHING@users.noreply.github.com>
2026-07-26 16:44:52 -0500

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,22 @@
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+        
+
+    def get(self, key: int) -> int:
+        if self.cache.get(key):
+            self.cache.move_to_end(key)
+            return self.cache.get(key)
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if(len(self.cache) > self.cap):
+            self.cache.popitem(last=False)
+        
